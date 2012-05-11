ff72c516126f439f947794870d2eeeef1788f90b
Patrik Åkerfeldt <patrik.akerfeldt@gmail.com>
2012-05-11 23:09:52 +0200

    Initial commit

---
+++ backend/src/main/scala/se/citerus/lookingfor/backend/HelloScalatra.scala
@@ -0,0 +1,53 @@
+package se.citerus.lookingfor.backend
+
+import org.scalatra._
+import com.mongodb.casbah.MongoConnection
+import com.mongodb.casbah.commons.MongoDBObject
+
+class LookingForServlet extends ScalatraServlet with UrlSupport {
+
+  get("/") {
+    <html>
+      <head>
+        <title>Looking For</title>
+      </head>
+      <body>
+        <h1>Looking For - A missing person search utility by Citerus</h1>
+      </body>
+    </html>
+  }
+
+  get("/:object/:user/footprints") {
+    val lat: String = params.getOrElse("lat", halt(400))
+    val lon: String = params.getOrElse("lon", halt(400))
+    val hash: String = params.getOrElse("verhash", halt(400))
+    val timestamp = System.currentTimeMillis();
+    val objectId = params("object")
+    val user = params("user")
+
+    val footprints = MongoConnection()("lookingfor")("footprints")
+
+    val query = MongoDBObject("object" -> objectId, "user" -> user);
+    val footprint = MongoDBObject(
+        "$push" -> MongoDBObject(
+    	    "footprints" -> MongoDBObject(
+    		    "timestamp" -> timestamp, 
+    			"loc" -> MongoDBObject(
+    			    "lat" -> lat, 
+    			    "lon" -> lon))));
+
+    footprints.update(query, footprint, true, false)
+
+    """{ "response" : "success" }"""
+  }
+
+  /*
+
+
+	{ object : "hattar", "user" : "my user", footprints : [{lat, lon, timestamp}] }
+
+
+  * 
+  */
+
+}

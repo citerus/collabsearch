0c68c5ec665cc6f41a2d7562e00414f677c9ed9d
orende <ola@ubuntu.(none)>
2012-06-26 09:57:50 +0200

    renaming packages

--- javabackend/admin-ui/src/main/java/se/citerus/WelcomeView.java
+++ javabackend/admin-ui/src/main/java/se/citerus/WelcomeView.java
@@ -1,6 +1,9 @@
 package se.citerus;
 
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
@@ -9,7 +12,17 @@ public class WelcomeView extends CustomComponent {
 	public WelcomeView() {
 		setCaption("Välkommen!");
 		VerticalLayout layout = new VerticalLayout();
-		layout.addComponent(new Label("Inloggningen lyckades"));
+		Label loginSuccessLabel = new Label("Inloggningen lyckades");
+		layout.addComponent(loginSuccessLabel);
+		layout.setComponentAlignment(loginSuccessLabel, Alignment.MIDDLE_CENTER);
+		HorizontalLayout subLayout = new HorizontalLayout();
+		Button userButton = new Button("Användare");
+		Button logoutButton = new Button("Logga ut");
+		Button searchMissionButton = new Button("Sökuppdrag");
+		subLayout.addComponent(userButton);
+		subLayout.addComponent(logoutButton);
+		subLayout.addComponent(searchMissionButton);
+		layout.addComponent(subLayout);
 		setCompositionRoot(layout);
 	}
 }
--- javabackend/admin-ui/src/main/java/se/citerus/logic/Authenticator.java
+++ javabackend/admin-ui/src/main/java/se/citerus/logic/Authenticator.java
@@ -1,6 +1,7 @@
 package se.citerus.logic;
 
 import com.mongodb.MongoException;
+import com.mongodb.util.Hash;
 
 import se.citerus.DAL.UserDAL;
 
@@ -20,14 +21,19 @@ public class Authenticator {
 	 * @return true if username and password are matched, else false.
 	 */
 	public boolean login(String username, char[] password) {
-		return true;
-//		if (username == null || password == null) {
-//			return false;
-//		}
-//		return userDAL.findUser(username, password);
+		if (username == null || password == null) {
+			return false;
+		}
+		return userDAL.findUser(username, password);
+//		return true;
 	}
 	
 	public boolean isAuthorized(String username, String role) {
 		return userDAL.findUserWithRole(username, role);
+	}
+	
+	private String hashPassword(String password) {
+		Long l = Hash.hashBackwardLong(password);
+		return l.toString();
 	}
 }

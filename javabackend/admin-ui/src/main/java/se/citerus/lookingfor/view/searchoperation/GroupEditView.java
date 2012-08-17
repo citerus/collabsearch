b435aa1b85aba66c6f505c5d82ae4905b3af98fb
orende <ola@ubuntu.(none)>
2012-08-17 12:53:33 +0200

    Removed old admin-ui project

--- javabackend/admin-ui/src/main/java/se/citerus/lookingfor/view/searchoperation/GroupEditView.java
+++
@@ -1,7 +0,0 @@
-package se.citerus.lookingfor.view.searchoperation;
-
-import com.vaadin.ui.CustomComponent;
-
-public class GroupEditView extends CustomComponent {
-
-}
--- javabackend/admin-ui/src/main/java/se/citerus/lookingfor/view/searchoperation/ZoneEditView.java
+++
@@ -1,7 +0,0 @@
-package se.citerus.lookingfor.view.searchoperation;
-
-import com.vaadin.ui.CustomComponent;
-
-public class ZoneEditView extends CustomComponent {
-
-}
